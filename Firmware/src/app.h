44d3376cec08bd49ec896daadcb6a960e5e5c0ca
Josip Medved <jmedved@jmedved.com>
2021-08-07 19:01:09 -0700

    Adjusted to use 31kHz osc on 10F320

--- Firmware/src/app.h
+++ Firmware/src/app.h
@@ -16,16 +16,18 @@
     // CONFIG
     #pragma config IOSCFS = 4MHZ    // Internal Oscillator Frequency Select bit (4 MHz)
     #pragma config MCPU = OFF       // Master Clear Pull-up Enable bit (Pull-up disabled)
-    #pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+    #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT enabled)
     #pragma config CP = OFF         // Code protection bit (Code protection off)
     #pragma config MCLRE = ON       // GP3/MCLR Pin Function Select bit (GP3/MCLR pin function is MCLR)
+
+    #define _XTAL_FREQ 4000000
 
 #elif defined(_10F320) || defined(_10F322)
 
     // CONFIG
     #pragma config FOSC = INTOSC    // Oscillator Selection bits (INTOSC oscillator: CLKIN function disabled)
     #pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
-    #pragma config WDTE = ON        // Watchdog Timer Enable (WDT enabled)
+    #pragma config WDTE = OFF       // Watchdog Timer Disable (WDT disabled)
     #pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
     #pragma config MCLRE = ON       // MCLR Pin Function Select bit (MCLR pin function is MCLR)
     #pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
@@ -34,14 +36,14 @@
     #pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
     #pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
 
+    #define _XTAL_FREQ 31000
+
 #else
 
     #error "Only PIC10F20x PIC10F22x, and PIC10F32x devices are supported"
 
 #endif
 
-
-#define _XTAL_FREQ 4000000
 
 #define wait_20ms()   CLRWDT(); __delay_ms(20);
 #define wait_short()  CLRWDT(); __delay_ms(150);
@@ -52,6 +54,8 @@ void init(void) {
 
 #if defined(_10F200) || defined(_10F202) || defined(_10F204) || defined(_10F206) || defined(_10F220) || defined(_10F222)
     OPTION = 0b11011111;  // disabled T0CKI
+#elif defined(_10F320) || defined(_10F322)
+    OSCCONbits.IRCF = 0b000;  // 31 kHz (LFINTOSC)
 #endif
 
 }
